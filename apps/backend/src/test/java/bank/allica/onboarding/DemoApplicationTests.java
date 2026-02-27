124426ce40e5b7077c777d6838a17b039ffc00fb
sambhav2612 <sambhavjain2612@gmail.com>
2026-02-27 21:39:16 +0530

    feat(init): setup monorepo

---
+++ apps/backend/src/test/java/bank/allica/onboarding/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package bank.allica.onboarding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
